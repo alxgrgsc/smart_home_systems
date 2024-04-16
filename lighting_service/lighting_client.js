f72cafe3cdd8c193dc48b7d539edc90358033f80
alexno3 <alxgrgsc@gmail.com>
2024-04-16 20:35:35 +0100

    add inquire to dependencies, modify the CLI and try handle errors

--- lighting_service/lighting_client.js
+++ lighting_service/lighting_client.js
@@ -1,13 +1,7 @@
 const grpc = require('@grpc/grpc-js');
 const chalk = require('chalk');
 const protoLoader = require('@grpc/proto-loader');
-const readline = require('readline');
-
-//readline interface for user input
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const inquirer = require('inquirer');
 
 //load the protocol buffer definition
 const packageDefinition = protoLoader.loadSync('smart_lighting.proto', {});
@@ -16,31 +10,29 @@ const smartLighting = grpc.loadPackageDefinition(packageDefinition).smart_home;
 //create gRPC client
 const client = new smartLighting.Lighting('localhost:50051', grpc.credentials.createInsecure());
 
-//function to ask user a question and return a promise
-function askQuestion(query) {
-  return new Promise(resolve => rl.question(query, resolve));
-}
-
-//main
 async function main() {
   let continueQuery = true;
 
-  //loop until user exits
   do {
     console.log();
-    console.log(chalk.yellow('Select an operation:'));
-    console.log('1: Light Status');
-    console.log('2: Brightness Status');
-    console.log('3: Colour Status');
-    console.log('4: Turn On/Off');
-    console.log('5: Adjust Brightness');
-    console.log('6: Change Colour');
 
-    const choice = await askQuestion('Enter choice: ');
+    const { choice } = await inquirer.prompt({
+      type: 'list',
+      name: 'choice',
+      message: chalk.yellow('Select an operation:'),
+      choices: [
+        'Light Status',
+        'Brightness Status',
+        'Colour Status',
+        'Turn On/Off',
+        'Adjust Brightness',
+        'Change Colour',
+        new inquirer.Separator(),
+      ],
+    });
 
     switch (choice) {
-      //get lightStatus
-      case '1':
+      case 'Light Status':
         await new Promise((resolve, reject) => {
           client.LightStatus({}, (error, response) => {
             if (error) {
@@ -53,8 +45,7 @@ async function main() {
           });
         });
         break;
-      case '2':
-        //get brightnessStatus
+      case 'Brightness Status':
         await new Promise((resolve, reject) => {
           client.BrightnessStatus({}, (error, response) => {
             if (error) {
@@ -67,8 +58,7 @@ async function main() {
           });
         });
         break;
-      case '3':
-        //get colourStatus  
+      case 'Colour Status':
         await new Promise((resolve, reject) => {
           client.ColourStatus({}, (error, response) => {
             if (error) {
@@ -81,33 +71,52 @@ async function main() {
           });
         });
         break;
+      case 'Turn On/Off':
+        const { isOnInput } = await inquirer.prompt({
+          type: 'list',
+          name: 'isOnInput',
+          message: 'Turn light on or off:',
+          choices: ['On', 'Off'],
+        });
+        const isOn = isOnInput === 'On';
 
-      case '4':
-        //turn light on/off
-        const isOnInput = await askQuestion('Turn light on (enter 1) or off (enter 0): ');
-        const isOn = isOnInput.trim() === '1'; // Convert input to boolean
-
-        if (isOnInput.trim() !== '0' && isOnInput.trim() !== '1') {
-          console.log('Invalid input. Please enter 0 or 1.');
-          break;
-        }
-
+        // Check current light status before changing
+        let currentStatus;
         await new Promise((resolve, reject) => {
-          client.ChangeLightStatus({ isOn }, (error, response) => {
+          client.LightStatus({}, (error, response) => {
             if (error) {
               console.error(error);
               reject(error);
             } else {
-              console.log(chalk.yellow(response.message));
+              currentStatus = response.isOn;
               resolve();
             }
           });
         });
-        break;
 
-      case '5':
-        //adjust brightness
-        const newBrightnessLevel = await askQuestion('Enter new brightness level(0-100): ');
+        if ((isOn && currentStatus === 'On') || (!isOn && currentStatus === 'Off')) {
+          console.log(chalk.yellow(`The light is already ${currentStatus}.`));
+        } else {
+          await new Promise((resolve, reject) => {
+            client.ChangeLightStatus({ isOn }, (error, response) => {
+              if (error) {
+                console.error(error);
+                reject(error);
+              } else {
+                console.log(chalk.yellow(response.message));
+                resolve();
+              }
+            });
+          });
+        }
+        break;
+      case 'Adjust Brightness':
+        const { newBrightnessLevel } = await inquirer.prompt({
+          type: 'input',
+          name: 'newBrightnessLevel',
+          message: 'Enter new brightness level(0-100):',
+        });
+
         await new Promise((resolve, reject) => {
           client.AdjustBrightness({ newBrightnessLevel: parseInt(newBrightnessLevel, 10) }, (error, response) => {
             if (error) {
@@ -120,10 +129,14 @@ async function main() {
           });
         });
         break;
+      case 'Change Colour':
+        const { newColour } = await inquirer.prompt({
+          type: 'list',
+          name: 'newColour',
+          message: 'Enter new colour:',
+          choices: ['white', 'red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink'],
+        });
 
-      case '6':
-        //change colour
-        const newColour = await askQuestion('Enter new colour (white, red, green, blue, yellow, orange, purple, pink): ');
         await new Promise((resolve, reject) => {
           client.ChangeColour({ colour: newColour }, (error, response) => {
             if (error) {
@@ -137,26 +150,18 @@ async function main() {
         });
         break;
       default:
-        console.log('Invalid choice. Please select a number between 1 and 6.');
+        console.log(chalk.red('Invalid choice. Please select an option from the list.'));
         break;
     }
-    //ask user if they want to continue
-    let validAnswer = false;
-    let anotherQuery = '';
-    while (!validAnswer) {
-      anotherQuery = await askQuestion('Do you want to select another query? (y/n): ');
-      if (anotherQuery.toLowerCase() === 'y' || anotherQuery.toLowerCase() === 'n') {
-        validAnswer = true;
-      } else {
-        console.log('Invalid input. Please enter "y" or "n".');
-      }
-    }
-    continueQuery = anotherQuery.toLowerCase() === 'y';
-  } while (continueQuery);
 
-  //close readline interface
-  rl.close();
+    const { anotherQuery } = await inquirer.prompt({
+      type: 'confirm',
+      name: 'anotherQuery',
+      message: 'Do you want to select another query?',
+    });
+
+    continueQuery = anotherQuery;
+  } while (continueQuery);
 }
 
-//call main function
 main();
