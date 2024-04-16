5f751a1ce6e48540f3e77bbfd79ebbdbf9f18be2
alexno3 <alxgrgsc@gmail.com>
2024-04-16 18:16:34 +0100

    create doorbell_server.js

--- doorbell_service/doorbell_server.js
+++ doorbell_service/doorbell_server.js
@@ -4,10 +4,10 @@ const PROTO_PATH = "smart_doorbell.proto";
 const packageDefinition = protoLoader.loadSync(PROTO_PATH);
 const smartHomeProto = grpc.loadPackageDefinition(packageDefinition).smart_home;
 
-// Initial state
+//default state 
 let silentMode = false;
 
-// Define gRPC service method for LiveVideoFeed
+//gRPC service method for LiveVideoFeed
 function liveVideoFeed(call) {
   console.log('Request for Live Video Feed received');
   const videoUrl = "https://rb.gy/85vh2p";
@@ -17,7 +17,7 @@ function liveVideoFeed(call) {
   call.end();
 }
 
-// Define gRPC service method for TodaysEvents
+//gRPC service method for TodaysEvents
 function todaysEvents(call) {
   console.log('Request for Today\'s Events received');
   const events = ["#1 event", "#2 event", "#3 event"];
@@ -30,13 +30,13 @@ function todaysEvents(call) {
   call.end();
 }
 
-// Define gRPC service method for SilentModeStatus
+//gRPC service method for SilentModeStatus
 function silentModeStatus(call, callback) {
   console.log('Request for Silent Mode Status received');
   callback(null, { isSilentModeOn: silentMode });
 }
 
-// Define gRPC service method for ToggleSilentMode
+//gRPC service method for ToogleSilentMode
 function toggleSilentMode(call, callback) {
   console.log('Request for Toggling Silent Mode received');
   silentMode = call.request.toggle;
@@ -45,10 +45,10 @@ function toggleSilentMode(call, callback) {
   callback(null, { message });
 }
 
-// Create gRPC server
+//create gRPC server
 const server = new grpc.Server();
 
-// Add gRPC service methods to the server
+//add gRPC service methods to the server
 server.addService(smartHomeProto.Doorbell.service, {
     LiveVideoFeed: liveVideoFeed,
     TodaysEvents: todaysEvents,
@@ -56,7 +56,7 @@ server.addService(smartHomeProto.Doorbell.service, {
     ToggleSilentMode: toggleSilentMode
 });
 
-// Bind and start the server
+//bind and start server
 server.bindAsync('127.0.0.1:50052', grpc.ServerCredentials.createInsecure(), (err, port) => {
     if (err) {
         console.error('Error starting server:', err);
