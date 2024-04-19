87cb320aef10699629fdf948f356e2d906ff8660
alexno3 <alxgrgsc@gmail.com>
2024-04-19 09:53:54 +0100

    finish doorbell_client.js and start thermostat service

--- doorbell_service/doorbell_client.js
+++ doorbell_service/doorbell_client.js
@@ -1,13 +1,7 @@
 const grpc = require('@grpc/grpc-js');
 const chalk = require('chalk');
 const protoLoader = require('@grpc/proto-loader');
-const readline = require('readline');
-
-//readline interface for user input
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const inquirer = require('inquirer');
 
 //load the protocol buffer definition
 const packageDefinition = protoLoader.loadSync('smart_doorbell.proto', {});
@@ -16,141 +10,119 @@ const smartDoorbell = grpc.loadPackageDefinition(packageDefinition).smart_home;
 //create gRPC client
 const client = new smartDoorbell.Doorbell('localhost:50052', grpc.credentials.createInsecure());
 
-//function to ask user a question and return a promise
-function askQuestion(query) {
-  return new Promise(resolve => rl.question(query, resolve));
-}
-
-//main
 async function main() {
   let continueQuery = true;
 
-
-  //loop until user exits
   do {
     console.log();
-    console.log(chalk.yellow('Select an operation:'));
-    console.log('1: Live Video Feed');
-    console.log('2: Today\'s Events');
-    console.log('3: Silent Mode Status');
-    console.log('4: Toggle Silent Mode');
 
-    const choice = await askQuestion('Enter choice: ');
-    console.log();
+    const { choice } = await inquirer.prompt({
+      type: 'list',
+      name: 'choice',
+      message: chalk.yellow('Select an operation:'),
+      choices: [
+        'Live Video Feed',
+        'Today\'s Events',
+        'Silent Mode Status',
+        'Toggle Silent Mode',
+        new inquirer.Separator(),
+      ],
+    });
 
     switch (choice) {
-      case '1':
-        try {
-          //live video feed
-          await new Promise((resolve, reject) => {
-            const liveVideoFeedStream = client.LiveVideoFeed({});
-            liveVideoFeedStream.on('data', response => {
-              if (response.liveVideoUrl) {
-                console.log('Live Video Feed URL:', chalk.yellow(response.liveVideoUrl));
-              }
-              if (response.description) {
-                console.log('Description:', chalk.yellow(response.description));
-                console.log();
-              }
-            });
-            liveVideoFeedStream.on('error', error => {
+      case 'Live Video Feed':
+        //live video feed
+        await new Promise((resolve, reject) => {
+          const liveVideoFeedStream = client.LiveVideoFeed({});
+          liveVideoFeedStream.on('data', response => {
+            if (response.liveVideoUrl) {
+              console.log('Live Video Feed URL:', chalk.yellow(response.liveVideoUrl));
+            }
+            if (response.description) {
+              console.log('Description:', chalk.yellow(response.description));
+              console.log();
+            }
+          });
+          liveVideoFeedStream.on('error', error => {
+            reject(error);
+          });
+          liveVideoFeedStream.on('end', () => {
+            console.log('End of Live Video Feed');
+            console.log();
+            resolve();
+          });
+        });
+        break;
+      case 'Today\'s Events':
+        //today's events
+        await new Promise((resolve, reject) => {
+          const todaysEventsStream = client.TodaysEvents({});
+          todaysEventsStream.on('data', event => {
+            console.log();
+            console.log('Event:', chalk.yellow(event.eventStatement));
+          });
+          todaysEventsStream.on('error', error => {
+            reject(error);
+          });
+          todaysEventsStream.on('end', () => {
+            console.log();
+            console.log('End of events');
+            resolve();
+          });
+        });
+        break;
+      case 'Silent Mode Status':
+        //silent mode status
+        await new Promise((resolve, reject) => {
+          client.SilentModeStatus({}, (error, response) => {
+            if (error) {
+              console.error(error);
               reject(error);
-            });
-            liveVideoFeedStream.on('end', () => {
-              console.log('End of Live Video Feed');
+            } else {
+              const status = response.isSilentModeOn ? 'on' : 'off';
+              console.log('Silent Mode Status:', chalk.yellow(status));
               console.log();
               resolve();
-            });
+            }
           });
-        } catch (error) {
-          console.error('Error handling Live Video Feed:', error);
-        }
+        });
         break;
-      case '2':
-        try {
-          //today's events
-          await new Promise((resolve, reject) => {
-            const todaysEventsStream = client.TodaysEvents({});
-            todaysEventsStream.on('data', event => {
-              console.log();
-              console.log('Event:', chalk.yellow(event.eventStatement));
-            });
-            todaysEventsStream.on('error', error => {
+      case 'Toggle Silent Mode':
+        const { toggleInput } = await inquirer.prompt({
+          type: 'list',
+          name: 'toggleInput',
+          message: 'Toggle silent mode:',
+          choices: ['On', 'Off'],
+        });
+        const toggle = toggleInput === 'On';
+
+        //toggle silent mode 
+        await new Promise((resolve, reject) => {
+          client.ToggleSilentMode({ toggle }, (error, response) => {
+            if (error) {
+              console.error(error);
               reject(error);
-            });
-            todaysEventsStream.on('end', () => {
+            } else {
+              console.log(chalk.yellow(response.message));
               console.log();
-              console.log('End of events');
               resolve();
-            });
+            }
           });
-        } catch (error) {
-          console.error('Error handling TodaysEvents:', error);
-        }
-        break;
-      case '3':
-        try {
-          //silent mode status
-          await new Promise((resolve, reject) => {
-            client.SilentModeStatus({}, (error, response) => {
-              if (error) {
-                console.error(error);
-                reject(error);
-              } else {
-                const status = response.isSilentModeOn ? 'on' : 'off';
-                console.log('Silent Mode Status:', chalk.yellow(status));
-                console.log();
-                resolve();
-              }
-            });
-          });
-        } catch (error) {
-          console.error('Error getting Silent Mode Status:', error);
-        }
-        break;
-      case '4':
-        try {
-          //toggle silent mode 
-          const toggleInput = await askQuestion('Toggle silent mode on (enter 1) or off (enter 0): ');
-          const toggle = toggleInput.trim() === '1';
-          await new Promise((resolve, reject) => {
-            client.ToggleSilentMode({ toggle }, (error, response) => {
-              if (error) {
-                console.error(error);
-                reject(error);
-              } else {
-                console.log(chalk.yellow(response.message));
-                console.log();
-                resolve();
-              }
-            });
-          });
-        } catch (error) {
-          console.error('Error toggling Silent Mode:', error);
-        }
+        });
         break;
       default:
-        console.log('Invalid choice. Please select a number between 1 and 4.');
+        console.log(chalk.red('Invalid choice. Please select an option from the list.'));
         break;
     }
 
-    //ask user to continue querying
-    let validAnswer = false;
-    let anotherQuery = '';
-    while (!validAnswer) {
-      anotherQuery = await askQuestion('Do you want to select another query? (y/n): ');
-      if (anotherQuery.toLowerCase() === 'y' || anotherQuery.toLowerCase() === 'n') {
-        validAnswer = true;
-      } else {
-        console.log('Invalid input. Please enter "y" or "n".');
-      }
-    }
-    continueQuery = anotherQuery.toLowerCase() === 'y';
-  } while (continueQuery);
+    const { anotherQuery } = await inquirer.prompt({
+      type: 'confirm',
+      name: 'anotherQuery',
+      message: 'Do you want to select another query?',
+    });
 
-  //close readline interface
-  rl.close();
+    continueQuery = anotherQuery;
+  } while (continueQuery);
 }
 
-//call main to start
 main();
