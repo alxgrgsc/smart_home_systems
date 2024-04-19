30110af8dbb43965a9dbca47b68287f46f2fb6f2
alexno3 <alxgrgsc@gmail.com>
2024-04-19 12:52:28 +0100

    implementing the boost options for the server and client side

---
+++ thermostat_service/thermostat_client.js
@@ -0,0 +1,200 @@
+const grpc = require('@grpc/grpc-js');
+const chalk = require('chalk');
+const protoLoader = require('@grpc/proto-loader');
+const inquirer = require('inquirer');
+
+//load the protocol buffer definition
+const packageDefinition = protoLoader.loadSync('smart_thermostat.proto', {});
+const smartThermostat = grpc.loadPackageDefinition(packageDefinition).smart_home;
+
+//create a new client instance
+const client = new smartThermostat.Thermostat('localhost:50053', grpc.credentials.createInsecure());
+
+async function main() {
+  let continueQuery = true;
+
+  do {
+    console.log();
+
+    const { choice } = await inquirer.prompt({
+      type: 'list',
+      name: 'choice',
+      message: chalk.yellow('Select an operation:'),
+      choices: [
+        'Temperature Status',
+        'Set Temperature',
+        'Boost Status',
+        'Manage Boost',
+        'Hot Water Status',
+        'Set Hot Water',
+        new inquirer.Separator(),
+      ],
+    });
+
+    switch (choice) {
+      case 'Temperature Status':
+        await new Promise((resolve, reject) => {
+          client.TemperatureStatus({}, (error, response) => {
+            if (error) {
+              console.error(error);
+              reject(error);
+            } else {
+              console.log('Temperature Status:', chalk.yellow(response.currentTemperature));
+              resolve();
+            }
+          });
+        });
+        break;
+      case 'Set Temperature':
+        const { setTemperature } = await inquirer.prompt({
+          type: 'input',
+          name: 'setTemperature',
+          message: 'Enter new temperature:',
+          validate: function (value) {
+            var valid = !isNaN(parseFloat(value));
+            return valid || 'Please enter a valid number';
+          },
+        });
+
+        await new Promise((resolve, reject) => {
+          client.SetTemperature({ setTemperature: parseFloat(setTemperature) }, (error, response) => {
+            if (error) {
+              console.error(error);
+              reject(error);
+            } else {
+              console.log(chalk.yellow(response.message));
+              resolve();
+            }
+          });
+        });
+        break;
+      case 'Boost Status':
+        await new Promise((resolve, reject) => {
+          client.BoostStatus({}, (error, response) => {
+            if (error) {
+              console.error(error);
+              reject(error);
+            } else {
+              let boostStatusMessage = 'Boost Status: ';
+              if (response.isBoostActive) {
+                boostStatusMessage += `On ${response.boostTemperature} degrees ${response.boostTimeRemaining} minutes`;
+              } else {
+                boostStatusMessage += 'Off';
+              }
+              console.log(chalk.yellow(boostStatusMessage));
+              resolve();
+            }
+          });
+        });
+        break;
+      case 'Manage Boost':
+        const { manageBoostChoice } = await inquirer.prompt({
+          type: 'list',
+          name: 'manageBoostChoice',
+          message: 'Select an operation:',
+          choices: ['Set Boost On/Off', 'Adjust Boost'],
+        });
+
+        switch (manageBoostChoice) {
+          case 'Set Boost On/Off':
+            const { setBoost } = await inquirer.prompt({
+              type: 'list',
+              name: 'setBoost',
+              message: 'Turn boost on or off:',
+              choices: ['On', 'Off'],
+            });
+
+            await new Promise((resolve, reject) => {
+              client.SetBoost({ setBoost: setBoost === 'On' }, (error, response) => {
+                if (error) {
+                  console.error(error);
+                  reject(error);
+                } else {
+                  console.log(chalk.yellow(response.message));
+                  resolve();
+                }
+              });
+            });
+            break;
+          case 'Adjust Boost':
+            const { boostTemperature, boostTime } = await inquirer.prompt([{
+              type: 'input',
+              name: 'boostTemperature',
+              message: 'Enter boost temperature:',
+              validate: function(value) {
+                var valid = !isNaN(parseFloat(value));
+                return valid || 'Please enter a valid number';
+              },
+            }, {
+              type: 'input',
+              name: 'boostTime',
+              message: 'Enter boost time:',
+              validate: function(value) {
+                var valid = !isNaN(parseFloat(value));
+                return valid || 'Please enter a valid number';
+              },
+            }]);
+
+            await new Promise((resolve, reject) => {
+              client.ManageBoost({ boostTemperature: parseFloat(boostTemperature), boostTime: parseFloat(boostTime) }, (error, response) => {
+                if (error) {
+                  console.error(error);
+                  reject(error);
+                } else {
+                  console.log(chalk.yellow(response.message));
+                  resolve();
+                }
+              });
+            });
+            break;
+        }
+        break;
+      case 'Hot Water Status':
+        await new Promise((resolve, reject) => {
+          client.HotWaterStatus({}, (error, response) => {
+            if (error) {
+              console.error(error);
+              reject(error);
+            } else {
+              console.log('Hot Water Status:', chalk.yellow(response.isHotWaterOn));
+              resolve();
+            }
+          });
+        });
+        break;
+      case 'Set Hot Water':
+        const { setHotWater } = await inquirer.prompt({
+          type: 'list',
+          name: 'setHotWater',
+          message: 'Turn hot water on or off:',
+          choices: ['On', 'Off'],
+        });
+
+        await new Promise((resolve, reject) => {
+          client.SetHotWater({ setHotWater: setHotWater === 'On' }, (error, response) => {
+            if (error) {
+              console.error(error);
+              reject(error);
+            } else {
+              console.log(chalk.yellow(response.message));
+              resolve();
+            }
+          });
+        });
+        break;
+      default:
+        console.log(chalk.red('Invalid choice. Please select an option from the list.'));
+        break;
+    }
+
+    const { anotherQuery } = await inquirer.prompt({
+      type: 'confirm',
+      name: 'anotherQuery',
+      message: 'Do you want to select another query?',
+    });
+
+    continueQuery = anotherQuery;
+  } while (continueQuery);
+}
+
+main();
---
+++ thermostat_service/thermostat_server.js
@@ -0,0 +1,91 @@
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+
+//load the protocol buffer definition
+const packageDefinition = protoLoader.loadSync('smart_thermostat.proto', {});
+const smartThermostat = grpc.loadPackageDefinition(packageDefinition).smart_home;
+
+// Default states
+let currentTemperature = 20.5;
+let boostTemperature = currentTemperature + 2;
+let isBoostActive = false;
+let boostTimeRemaining = 15;
+let isHotWaterOn = false;
+
+//create gRPC server
+const server = new grpc.Server();
+
+//implement the service methods
+server.addService(smartThermostat.Thermostat.service, {
+  TemperatureStatus: (call, callback) => {
+    console.log('Request for Temperature Status received');
+    console.log('Sending Temperature Status:', currentTemperature);
+    callback(null, { currentTemperature: currentTemperature });
+  },
+  SetTemperature: (call, callback) => {
+    console.log('Request to set temperature received');
+    currentTemperature = call.request.setTemperature;
+    console.log('Temperature set to:', currentTemperature);
+    callback(null, { message: `Temperature set to ${currentTemperature}` });
+  },
+  BoostStatus: (call, callback) => {
+    console.log('Request for Boost Status received');
+    let boostStatusMessage = 'Boost Status: ';
+    if (isBoostActive) {
+      boostStatusMessage += `On ${boostTemperature} degrees ${boostTimeRemaining} minutes`;
+    } else {
+      boostStatusMessage += 'Off';
+    }
+    console.log(boostStatusMessage);
+    callback(null, { isBoostActive: isBoostActive, boostTemperature: boostTemperature, boostTimeRemaining: boostTimeRemaining });
+  },
+  SetBoost: (call, callback) => {
+    console.log('Request to set boost received');
+    isBoostActive = call.request.setBoost;
+    if (!isBoostActive) {
+      currentTemperature = 20.5;
+      boostTimeRemaining = 30;
+    }
+    console.log('Boost set to:', isBoostActive ? 'On' : 'Off');
+    callback(null, { message: `Boost set to ${isBoostActive ? 'On' : 'Off'}` });
+  },
+  AdjustBoost: (call, callback) => {
+    console.log('Request to adjust boost received');
+    isBoostActive = true;
+    currentTemperature = call.request.boostTemperature;
+    boostTimeRemaining = call.request.boostTime;
+    console.log(`Boost adjusted. Temperature: ${currentTemperature}, Time remaining: ${boostTimeRemaining}`);
+    callback(null, { message: `Boost adjusted. Temperature: ${currentTemperature}, Time remaining: ${boostTimeRemaining}` });
+  },
+  HotWaterStatus: (call, callback) => {
+    console.log('Request for Hot Water Status received');
+    console.log('Sending Hot Water Status:', isHotWaterOn);
+    callback(null, { isHotWaterOn: isHotWaterOn });
+  },
+  SetHotWater: (call, callback) => {
+    console.log('Request to set hot water status received');
+    isHotWaterOn = call.request.setHotWater;
+    console.log('Hot water status set to:', isHotWaterOn);
+    callback(null, { message: `Hot water status set to ${isHotWaterOn}` });
+  },
+  ManageBoost: (call, callback) => {
+    console.log('Request to manage boost received');
+    if (call.request.boostTemperature && call.request.boostTime) {
+      isBoostActive = true;
+      currentTemperature = call.request.boostTemperature;
+      boostTimeRemaining = call.request.boostTime;
+      console.log(`Boost adjusted. Temperature: ${currentTemperature}, Time remaining: ${boostTimeRemaining}`);
+      callback(null, { message: `Boost adjusted. Temperature: ${currentTemperature}, Time remaining: ${boostTimeRemaining}` });
+    } else {
+      callback({
+        code: grpc.status.INVALID_ARGUMENT,
+        message: 'Invalid request. boostTemperature and boostTime are required to manage boost.',
+      });
+    }
+  },
+});
+
+//start the server
+server.bindAsync('localhost:50053', grpc.ServerCredentials.createInsecure(), () => {
+  console.log('Server running on http://localhost:50053');
+});
